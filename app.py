2ec60e7100cf7cfadf7706a26bf99d6b77619dc4
Bindhu Madhavi Menda <121569832+Bindhu-478965@users.noreply.github.com>
2023-07-30 18:36:23 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,33 @@
+import numpy as np
+from flask import Flask, request, render_template
+import pickle
+app=Flask(__name__,template_folder='templates')
+model=pickle.load(open('C:/Users/M9bin/OneDrive/Documents/brainstroke/models/model.pkl','rb'))
+'''
+with open('C:/Users/M9bin/OneDrive/Documents/brainstroke/models/model.pkl', 'rb') as file:
+    model = pickle.load(file)
+'''
+@app.route('/')
+def home():
+    return render_template('brain.html')
+@app.route('/predict',methods=['POST'])
+def predict():
+    features = request.form.values()
+    processed_features = []
+
+    for i in features:
+        try:
+            # Try converting to float (integers will also be converted to float)
+            processed_features.append(float(i))
+        except ValueError:
+            # If it's not a valid float, keep it as a string (for categorical features)
+            processed_features.append(i)
+
+    features = [np.array(processed_features)]
+    prediction = model.predict(features)
+    output = round(prediction[0][0], 3)  # Assuming the model output is a 2D array
+
+    return render_template('brain.html', prediction_text='The prediction value is {}'.format(output))
+if __name__=="__main__":    
+    app.run()
+            
